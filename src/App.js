991e73fbe612b63889b2f08ce8b4c66076943b8b
prakharjain24 <prakharjain995@gmail.com>
2022-09-25 19:45:33 +0530

    apply routing and integration of styled component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from './About';
+import Products from "./Products";
+import Contact from "./Contact";
+import Cart from "./Cart";
+import SingleProduct from "./SingleProduct";
+import ErrorPage from './ErrorPage';
+import { GlobalStyle } from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  const theme = {
+    colors: {
+      heading: "",
+      text: "rgba(29, 29, 29, .8)",
+      white: "#fff",
+      black: "212529",
+      helper: "#8490ff",
+      bg: "#000"
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+    <Router>
+    {/* <GlobalStyle> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About /> } />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} /> 
+        <Route path="*" element={<ErrorPage />} /> 
+      </Routes>
+    {/* </GlobalStyle> */}
+    </Router>
+    </ThemeProvider>
+  );
 };
 
 export default App;
